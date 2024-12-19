8ec1cf2619d7b911f1f4e8cf03ef79d40fad6b55
DibyansuBiswal <dibyansu.biswal2005@gmail.com>
2024-12-19 22:17:31 +0530

    First Commit

---
+++ Day43.c
@@ -0,0 +1,71 @@
+// C program to merge a linked list into another at
+// alternate positions
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+
+void printList(struct Node *head) {
+    struct Node *curr = head;
+    while (curr != NULL) {
+        printf("%d ", curr->data);
+        curr = curr->next;
+    }
+    printf("\n");
+}
+
+// Function to merge two linked lists
+struct Node *merge(struct Node *head1, struct Node *head2) {
+    struct Node *curr1 = head1;
+    struct Node *curr2 = head2;
+    struct Node *next1, *next2;
+
+    // Traverse both lists and merge them
+    while (curr1 != NULL && curr2 != NULL) {
+
+        next1 = curr1->next;
+        next2 = curr2->next;
+
+        curr2->next = curr1->next;
+        curr1->next = curr2;
+
+        curr1 = next1;
+        curr2 = next2;
+    }
+
+    return curr2;
+}
+
+struct Node *createNode(int data) {
+    struct Node *newNode = 
+      (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+int main() {
+  
+    // Creating the first linked list: 1->2->3
+    struct Node *head1 = createNode(1);
+    head1->next = createNode(2);
+    head1->next->next = createNode(3);
+
+    // Creating the second linked list: 4->5->6->7->8
+    struct Node *head2 = createNode(4);
+    head2->next = createNode(5);
+    head2->next->next = createNode(6);
+    head2->next->next->next = createNode(7);
+    head2->next->next->next->next = createNode(8);
+
+    struct Node *remaining = merge(head1, head2);
+
+    printList(head1);
+    printList(remaining);
+
+    return 0;
+}
